dfdc2ab686ceda67c324b13241fad428168c59f6
Amr Aboushousha <amrtariq@gmail.com>
2022-08-19 23:21:13 +0200

    save no predict

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def get_output():
     if request.method == 'POST':
         img = request.files['my_image']
         img_path = "static/" + img.filename	
-# 		 img.save(img_path)
+        img.save(img_path)
 
 # 		 p = predict_label(img_path)
 
